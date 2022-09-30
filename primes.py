520ef177fb8508d577487d61d8ce38cf294bd921
Reuben Atendido <K21048267@kcl.ac.uk>
2022-09-30 15:35:57 +0100

    implement the calculation of primes up until the upperbound provided

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+    
+    count = 0
+    for x in range(2,number_of_primes):
+        prime = True
+        for i in range(2,x):
+            if x % i == 0:
+                prime = False 
+        if prime:
+            if count != number_of_primes:
+                list.append(x)
+                count+=1
+            else:
+                break
+            
+            
+        
     return list
+
+# x=input("enter: ")
+# print(primes(int(x)))
